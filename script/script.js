b9833d8982895b081894084e2b70cb710fa4d388
kimHS <aptlf153@naver.com>
2021-07-24 14:42:19 +0900

    모바일 반응형 구현중

--- script/script.js
+++ script/script.js
@@ -69,8 +69,6 @@ window.addEventListener('resize',function(){
     aniheight = animation.offsetHeight;
     careerheight = career.offsetHeight;
     endheight = PORTFOLIO.offsetHeight+careerheight+aniheight;
-    
-    console.log(careerheight)
 });
 
 navbarlist[0].addEventListener('click',function(){
@@ -81,12 +79,54 @@ navbarlist[1].addEventListener('click',function(){
     window.scrollTo({top:aniheight, left:0, behavior:'smooth'});             
 });
 
-
 navbarlist[2].addEventListener('click',function(){
-  
-        window.scrollTo({top:aniheight+careerheight, left:0, behavior:'smooth'});
+     window.scrollTo({top:aniheight+careerheight, left:0, behavior:'smooth'});
 });
 
 navbarlist[3].addEventListener('click',function(){
    window.scrollTo({top:endheight, left:0, behavior:'smooth'});   
 });
+
+navbarlist[5].addEventListener('click',function(){
+    window.scrollTo({top:0, left:0, behavior:'auto'});  
+    mobilMeunClose(); 
+});
+
+navbarlist[6].addEventListener('click',function(){                   
+   window.scrollTo({top:aniheight, left:0, behavior:'auto'});    
+   mobilMeunClose();         
+});
+
+navbarlist[7].addEventListener('click',function(){
+       window.scrollTo({top:aniheight+careerheight, left:0, behavior:'auto'});
+       mobilMeunClose();
+});
+
+navbarlist[8].addEventListener('click',function(){
+  window.scrollTo({top:endheight, left:0, behavior:'auto'});  
+  mobilMeunClose(); 
+});
+
+
+const mobil = document.querySelector('.mobil');
+const navbarmenu = document.querySelector('.navbar_menu');
+const shade = document.querySelector('.shade');
+const mobil_navbar_menu = document.querySelector('.mobil_navbar_menu');
+
+mobil.addEventListener('click',function(){
+    shade.style.display = 'block';
+    mobil_navbar_menu.style.display = 'block';
+});
+
+function mobilMeunClose(){
+    shade.style.display = 'none';
+    mobil_navbar_menu.style.display = 'none';
+};
+
+navbarlist[4].addEventListener('click',function(){
+    mobilMeunClose();
+ });
+
+ shade.addEventListener('click',function(){
+    mobilMeunClose();
+ });
